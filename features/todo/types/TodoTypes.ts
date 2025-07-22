c467e5ef2f9ac606ab83a47f86460e09469f456e
Soyeon Lee <pink731p@gmail.com>
2025-07-22 20:31:16 +0900

    feat: todo 관련 타입 정의

---
+++ features/todo/types/TodoTypes.ts
@@ -0,0 +1,27 @@
+type ISODateString = string; // "2025-07-21"
+// type ISODateTimeString = string; // "2025-07-21T09:15:00Z"
+
+export interface Todo {
+  title: string;
+  targetDate: ISODateString;
+  completed: boolean;
+  categoryId: string;
+  alarm?: string;
+  dueTime?: string;
+  // priority?: "high" | "medium" | "low";
+  // dueDate?: string;
+}
+
+export interface CreatedTodo extends Todo {
+  id: string;
+  createdAt: ISODateString;
+  updatedAt?: ISODateString;
+}
+
+export interface Category {
+  id: string;
+  name: string;
+  color: string;
+  icon: string;
+  colorCircle: React.ReactNode;
+}
