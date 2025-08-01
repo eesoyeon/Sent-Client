fd9a15974e04a3eae40fe0d2104248a4626cdfcd
Soyeon Lee <pink731p@gmail.com>
2025-08-01 22:51:31 +0900

    refactor: todo 타입 api에 맞게 수정

--- src/entities/todos/types/TodoTypes.ts
+++ src/entities/todos/types/TodoTypes.ts
@@ -1,27 +1,26 @@
+import { Category } from '@/entities/todos/types/CategoryTypes';
+
 type ISODateString = string; // "2025-07-21"
-// type ISODateTimeString = string; // "2025-07-21T09:15:00Z"
 
 export interface Todo {
+  id: number;
   title: string;
-  scheduleDate: ISODateString;
-  completed: boolean;
-  categoryId: string;
-  alarm?: string;
-  dueTime?: string;
+  category: Category;
+  scheduledDate: ISODateString;
+  isDone: boolean;
+  scheduledTime: string | null;
+  notificationTime: string | null;
+  createdAt?: ISODateString;
+  updatedAt?: ISODateString;
   // priority?: "high" | "medium" | "low";
+  // dueTime?: string;
   // dueDate?: string;
 }
 
-export interface CreatedTodo extends Todo {
-  id: string;
-  createdAt: ISODateString;
-  updatedAt?: ISODateString;
-}
-
-export interface Category {
-  id: string;
-  name: string;
-  color: string;
-  icon: string;
-  colorCircle: React.ReactNode;
+export interface CreateTodoRequest {
+  title: string;
+  category: string;
+  scheduledDate: ISODateString;
+  scheduledTime: string | null;
+  notificationTime: string | null;
 }
