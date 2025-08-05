c158bf160631cbda858a59c37b19677666ecb18f
Soyeon Lee <pink731p@gmail.com>
2025-08-05 19:47:43 +0900

    fix: category 오타 수정

--- src/entities/todos/api/categoriesApi.ts
+++ src/entities/todos/api/categoriesApi.ts
@@ -2,13 +2,13 @@ import { Category, CreateCategoryRequest } from '@/entities/todos/types/Category
 import axiosInstance from '@/shared/lib/axios';
 
 export const getCategories = async (): Promise<Category[]> => {
-  const res = await axiosInstance.get(`/api/v1/cateogories`);
-  return res.data;
+  const res = await axiosInstance.get(`/api/v1/categories`);
+  return res.data.data;
 };
 
 export const getCategoryByName = async (name: string): Promise<Category> => {
   const encodeName = encodeURIComponent(name);
-  const res = await axiosInstance.get(`/api/v1/cateogories/search?name=${encodeName}`);
+  const res = await axiosInstance.get(`/api/v1/categories/search?name=${encodeName}`);
   return res.data;
 };
 
