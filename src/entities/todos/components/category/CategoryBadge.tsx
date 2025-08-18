15d0c3eb6b1d2ff0ad764851b4009cfddad856d1
Soyeon Lee <pink731p@gmail.com>
2025-08-18 22:11:58 +0900

    refactor: 카테고리 id 타입에 null 추가

--- src/entities/todos/components/category/CategoryBadge.tsx
+++ src/entities/todos/components/category/CategoryBadge.tsx
@@ -1,21 +1,21 @@
-import { Category } from '@/entities/todos/types/TodoTypes';
+import { Category } from '@/entities/todos/types/CategoryTypes';
 import { getIconComponent } from '@/shared/lib/icons';
 import React from 'react';
 
 interface CategoryBadgeProps {
-  category: Category | undefined;
+  category: Category;
 }
 
 const CategoryBadge = ({ category }: CategoryBadgeProps) => {
-  const CategoryIcon = getIconComponent(category?.icon || 'circle');
+  const CategoryIcon = getIconComponent(category.icon);
 
   return (
     <div
       className={`text-sm font-semibold flex items-center px-2 py-1 bg-gray-900 border border-gray-800 rounded-md backrop-blur-sm w-fit space-x-2`}
-      style={{ color: category?.color }}
+      style={{ color: category.color }}
     >
       {CategoryIcon && <CategoryIcon className="w-3 h-3 inline-block" />}
-      <span>{category?.name}</span>
+      <span>{category.name}</span>
     </div>
   );
 };
--- src/entities/todos/types/CategoryTypes.ts
+++ src/entities/todos/types/CategoryTypes.ts
@@ -1,5 +1,5 @@
 export interface Category {
-  id: number;
+  id: number | null;
   name: string;
   color: string;
   icon: string;
