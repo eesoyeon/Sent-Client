dd117dd6cbce5d5f455335752c53b38f33f07398
Soyeon Lee <pink731p@gmail.com>
2025-08-18 22:20:16 +0900

    feat: 인증 인터셉터 추가

--- src/shared/lib/axios.ts
+++ src/shared/lib/axios.ts
@@ -9,15 +9,24 @@ const axiosInstance = axios.create({
   withCredentials: true,
 });
 
+axiosInstance.interceptors.request.use(config => {
+  const token = localStorage.getItem('access_token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 // 응답 에러 처리
-// axiosInstance.interceptors.response.use(
-//   response => response,
-//   error => {
-//     if (error.response?.status === 401) {
-//       console.warn('인증 실패: 로그인 다시 필요');
-//     }
-//     return Promise.reject(error);
-//   },
-// );
+axiosInstance.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response?.status === 401) {
+      console.warn('인증 실패: 로그인 다시 필요');
+      window.location.href = '/auth';
+    }
+    return Promise.reject(error);
+  },
+);
 
 export default axiosInstance;
