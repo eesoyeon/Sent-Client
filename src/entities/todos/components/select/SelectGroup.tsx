2fdd16e806e39688faa2e21ba3bff5357f3c53b5
Soyeon Lee <pink731p@gmail.com>
2025-08-18 22:23:07 +0900

    feat: SelectGroup에서 SelectOptionWithCustomContent 지원 추가

--- src/entities/todos/components/select/SelectGroup.tsx
+++ src/entities/todos/components/select/SelectGroup.tsx
@@ -2,10 +2,10 @@ import SelectDropdownItem from '@/entities/todos/components/select/SelectDropdow
 import SelectRow from '@/entities/todos/components/select/SelectRow';
 import { useClickOutside } from '@/shared/hooks/useClickOutside';
 import { getDropdownItemStyle, getSelectItemStyle } from '@/entities/todos/utils/getFormItemStyle';
-import { useRef } from 'react';
+import { ReactNode, useRef } from 'react';
 
-export interface SelectOption {
-  id: number;
+export interface SelectOptionWithDropdown {
+  id: number | string;
   label: string;
   value: string | number | null;
   icon?: React.ReactNode;
@@ -15,12 +15,25 @@ export interface SelectOption {
     icon?: React.ReactNode;
   }[];
   onSelect?: (value: string | number) => void;
+  disabled?: boolean;
 }
+
+interface SelectOptionWithCustomContent {
+  id: number | string;
+  label: string;
+  value: string | number | null;
+  icon?: React.ReactNode;
+  customContent: ReactNode;
+  onSelect?: (value: string | number) => void;
+  disabled?: boolean;
+}
+
+export type SelectOption = SelectOptionWithDropdown | SelectOptionWithCustomContent;
 
 interface SelectGroupProps {
   options: SelectOption[];
-  openSelectId: number | null;
-  onToggle: (id: number | null) => void;
+  openSelectId: number | string | null;
+  onToggle: (id: number | string | null) => void;
 }
 
 const SelectGroup = ({ options, openSelectId, onToggle }: SelectGroupProps) => {
@@ -32,7 +45,7 @@ const SelectGroup = ({ options, openSelectId, onToggle }: SelectGroupProps) => {
   });
 
   return (
-    <div ref={wrapperRef}>
+    <div>
       {options.map((selectOption, optionIndex) => {
         const { border, rounded } = getSelectItemStyle(optionIndex, options.length);
         const isOpen = openSelectId === selectOption.id;
@@ -43,33 +56,43 @@ const SelectGroup = ({ options, openSelectId, onToggle }: SelectGroupProps) => {
               label={selectOption.label}
               value={selectOption.value}
               icon={selectOption.icon}
+              disabled={selectOption.disabled}
               isOpen={isOpen}
               className={`${border} ${rounded}`}
               onClick={() => onToggle(isOpen ? null : selectOption.id)}
             />
 
             {isOpen && (
-              <div className="absolute right-3 top-9 w-fit max-h-60 overflow-y-auto rounded-xl shadow-lg z-50 backdrop-blur-lg">
-                {selectOption.dropdownOptions.map((dropdownItem, dropdownIndex) => {
-                  const { border, rounded } = getDropdownItemStyle(
-                    dropdownIndex,
-                    selectOption.dropdownOptions.length,
-                  );
+              <div
+                ref={wrapperRef}
+                className="absolute right-3 top-9 w-fit max-h-60 overflow-y-auto rounded-xl shadow-lg z-50 backdrop-blur-lg "
+              >
+                {'customContent' in selectOption ? (
+                  <div className="p-2 rounded-xl border-t border-t-white/40 border-b border-b-white/40">
+                    {selectOption.customContent}
+                  </div>
+                ) : (
+                  selectOption.dropdownOptions?.map((dropdownItem, dropdownIndex) => {
+                    const { border, rounded } = getDropdownItemStyle(
+                      dropdownIndex,
+                      selectOption.dropdownOptions.length,
+                    );
 
-                  return (
-                    <SelectDropdownItem
-                      key={dropdownItem.value}
-                      label={dropdownItem.label}
-                      value={dropdownItem.value ?? ''}
-                      icon={dropdownItem.icon}
-                      className={`${border} ${rounded}`}
-                      onSelect={value => {
-                        selectOption.onSelect?.(value);
-                        onToggle(null);
-                      }}
-                    />
-                  );
-                })}
+                    return (
+                      <SelectDropdownItem
+                        key={dropdownItem.value}
+                        label={dropdownItem.label}
+                        value={dropdownItem.value ?? ''}
+                        icon={dropdownItem.icon}
+                        className={`${border} ${rounded}`}
+                        onSelect={value => {
+                          selectOption.onSelect?.(value);
+                          onToggle(null);
+                        }}
+                      />
+                    );
+                  })
+                )}
               </div>
             )}
           </div>
